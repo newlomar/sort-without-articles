8ac307689b2d3e8062c49a45f40b0f61b4ea741d
Newton Lomar <newtonlomar16@gmail.com>
2021-10-21 11:54:03 -0300

    first delivery

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-const bandsWithNoArticles = bands.map((band) => {
-
-  const splittedBandName = band.split(' ');
-  if (
-      splittedBandName[0].toLocaleLowerCase() === 'the' || 
-      splittedBandName[0].toLocaleLowerCase() === 'a' || 
-      splittedBandName[0].toLocaleLowerCase() === 'an'
-  ) {
-    const bandWithoutArticle = splittedBandName.slice(1);
-    return bandWithoutArticle;
-  }
-
-  return band;
-
-});
-
 const sortedBands = bands.sort((a, b) => {
 
-  console.log(a, b);
+  let nameBandA;
+  let nameBandB;
 
-  if (a > b) {
+  if (a.split(' ')[0].toLowerCase() === 'the' || a.split(' ')[0].toLowerCase() === 'a' || a.split(' ')[0].toLowerCase() === 'an') {
+
+    nameBandA = (a.split(' ')).slice(1).join(' ');
+
+  }
+
+  else {
+
+    nameBandA = a;
+
+  }
+
+  if (b.split(' ')[0].toLowerCase() === 'the' || b.split(' ')[0].toLowerCase() === 'a' || b.split(' ')[0].toLowerCase() === 'an') {
+
+    nameBandB = (b.split(' ')).slice(1).join(' ');
+
+  }
+
+  else {
+
+    nameBandB = b;
+
+  }
+
+  if (nameBandA > nameBandB) {
     return 1;
   }
 
-  if (a < b) {
+  if (nameBandA < nameBandB) {
     return -1;
   }
 
