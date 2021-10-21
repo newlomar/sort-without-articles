c0e63fad6af0ede97f8560d5a8acc78d1abc4dda
Newton Lomar <newtonlomar16@gmail.com>
2021-10-21 12:03:42 -0300

    adding alternative solution and commenting it

--- script.js
+++ script.js
@@ -49,3 +49,18 @@ sortedBands.forEach((band) => {
   li.textContent = `${band}`;
   ul.appendChild(li);
 });
+
+
+/* Alternative solution */
+
+// function strip(bandName) {
+
+//   return bandName.replace(/^(a |the |an )/i, '').trim();
+
+// }
+
+// const sortedBands = bands.sort((a,b) => strip(a) > strip(b) ? 1 : -1);
+
+// document.querySelector('#bands').innerHTML = 
+//   sortedBands.map(band => `<li>${band}</li>`)
+//   .join('');
